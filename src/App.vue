b8a495e04081efbbc9747cc340b2494f711307b7
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2022-03-30 21:33:15 +0800

    refactor(app): change padding to max-w-screen

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <NavBar class="px-8 md:px-24 xl:px-60"/>
-  <router-view class="px-8 md:px-24 xl:px-60"/>
-  <Footer class="px-8 md:px-24 xl:px-60"/>
+  <NavBar
+    class="px-8 lg:px-4 lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl mx-auto"
+  />
+  <router-view
+    class="px-8 lg:px-4 lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl mx-auto"
+  />
+  <Footer
+    class="px-8 lg:px-4 lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl mx-auto"
+  />
 </template>
 
 <script>
-import NavBar from '@/components/NavBar.vue'
-import Footer from '@/components/Footer.vue'
+import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   components: {
     NavBar,
     Footer,
-  }
-}
+  },
+};
 </script>
