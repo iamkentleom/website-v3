bc7b38e93b5e5a1304f4bc8714d8fdc3e63d010d
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2022-03-31 21:58:18 +0800

    fix(app): sticky footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <NavBar
-    class="px-8 lg:px-4 lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl mx-auto"
-  />
-  <router-view
-    class="px-8 lg:px-4 lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl mx-auto"
-  />
-  <Footer
-    class="px-8 lg:px-4 lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl mx-auto"
-  />
+  <div class="flex flex-col min-h-full">
+    <NavBar
+      class="w-full px-8 lg:px-4 lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl mx-auto"
+    />
+    <router-view
+      class="w-full px-8 lg:px-4 lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl mx-auto"
+    />
+    <Footer
+      class="w-full mt-auto px-8 lg:px-4 lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl mx-auto"
+    />
+  </div>
 </template>
 
 <script>
