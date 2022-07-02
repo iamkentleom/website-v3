0efec4b0dff3d53a133e6b5ea7306c89993cdd8d
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2022-07-02 11:56:54 +0800

    feat(ProjectCard): redesigned cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,24 +1,36 @@
 <template>
-  <div class="bg-gray-100 rounded-2xl overflow-hidden card">
-    <div class="overflow-hidden">
-      <img class="w-full h-44 object-cover" :src="link" alt="" />
-    </div>
-    <p class="uppercase text-sm text-teal-500 font-extrabold px-4 pt-2">
-      {{ type }}
-    </p>
-    <div class="flex justify-between items-start px-4">
-      <h1 class="text-2xl font-semibold">{{ title }}</h1>
-      <a
-        :href="url"
-        target="_blank"
-        rel="noreferrer"
-        class="hover:bg-gray-200 p-1 rounded-md"
-      >
-        <img class="w-6" src="@/assets/external-link.svg" alt="Link" />
-      </a>
-    </div>
-    <p class="px-4 pb-4 font-normal">{{ description }}</p>
-  </div>
+  <li class="rounded-2xl overflow-hidden">
+    <a :href="url" target="_blank" rel="noreferrer">
+      <div class="container">
+        <div class="overflow-hidden rounded-2xl">
+          <img
+            class="w-full h-60 object-cover rounded-2xl"
+            :src="link"
+            alt=""
+            loading="lazy"
+          />
+        </div>
+        <div class="overlay">
+          <div class="flex bg-teal-50 border p-4 rounded-2xl">
+            visit {{ title }} &ensp;
+            <img class="w-6" src="@/assets/external-link.svg" alt="Link" />
+          </div>
+        </div>
+      </div>
+      <div class="overflow-hidden"></div>
+      <div class="flex justify-between items-start px-4 pt-4">
+        <div class="flex items-center">
+          <h1 class="text-2xl font-semibold">{{ title }}</h1>
+          <p
+            class="bg-teal-100 uppercase text-xs text-teal-500 font-extrabold ml-2 px-3 py-1 rounded-full"
+          >
+            {{ type }}
+          </p>
+        </div>
+      </div>
+      <p class="px-4 pb-4 font-normal">{{ description }}</p>
+    </a>
+  </li>
 </template>
 
 <script>
@@ -46,12 +58,27 @@ export default {
 };
 </script>
 
-<style scoped>
-.card {
-  min-width: 20rem;
-  max-width: 20rem;
-  border: 0.5rem solid white;
-  border-radius: 1.5rem;
-  box-sizing: content-box;
+<style lang="postcss" scoped>
+.container {
+  @apply relative;
+}
+.overlay {
+  @apply hidden;
+  transition: 0.8s ease;
+}
+li:hover .overlay {
+  @apply absolute top-0 bottom-0 left-0 right-0;
+  @apply rounded-2xl;
+  @apply flex items-center justify-center;
+  transition: 0.8s ease;
+}
+.container > div > img {
+  transition: 0.8s ease;
+}
+li:hover div.container > div > img {
+  transform: scale(1.2);
+  filter: blur(4px);
+  -webkit-filter: blur(4px);
+  transition: 0.8s ease;
 }
 </style>
